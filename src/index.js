63e07901ec083e00c01a1f90d8a758fe3de48dec
Kipris <tnzaria@gmail.com>
2020-10-03 19:19:12 +0300

    feat: implement array methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let minimum = array[0]
+    for (let i = 1; i < array.length; i += 1) {
+        if (minimum > array[i]) {
+            minimum = array[i]
+        }
+    }
+    return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let maximum = array[0]
+    for (let i = 1; i < array.length; i += 1) {
+        if (maximum < array[i]) {
+            maximum = array[i]
+        }
+    }
+    return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < array.length; i += 1) {
+        sum += array[i];
+    }
+    const average = sum / array.length;
+    return average;
 }
